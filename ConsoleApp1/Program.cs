2ed970353d5076ddf349fbe9491fc6d998d67153
Xiu Jian <xiujian@shinetechsoftware.com>
2024-02-17 08:10:23 +0800

    333

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!123aaasss");
+Console.WriteLine("Hello, World!123aaasss333");
 int a = 5;
 Console.Read();
